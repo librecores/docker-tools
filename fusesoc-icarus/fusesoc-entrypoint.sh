85d6073d2787d200e3d7059329578e145b9f132c
Oleg Nenashev <o.v.nenashev@gmail.com>
2016-09-04 03:51:56 +0200

    Issue #3: Checkout actual core and system definitions on startup

--- fusesoc-icarus/fusesoc-entrypoint.sh
+++ fusesoc-icarus/fusesoc-entrypoint.sh
@@ -3,9 +3,11 @@ echo "Running FuseSoC in $HOME"
 
 # Init data folders
 mkdir /fusesoc/build
-mkdir /fusesoc/cores
-mkdir /fusesoc/systems
 mkdir /fusesoc/cache
+
+#TODO: Make the behavior flexible - source repo should be modifyable
+cd /fusesoc
+git clone https://github.com/openrisc/orpsoc-cores.git
 
 # We Initialize FuseSoC within the user environment. Otherwise there are conflicts
 # TODO: Ideally it should be moved to the build stage
